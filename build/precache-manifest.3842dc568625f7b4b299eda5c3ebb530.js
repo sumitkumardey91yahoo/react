49aa2456c4b94941b82a899485aae36243b67ebc
sumitkumar91 <sumit.kumar@mobiotics.com>
2019-05-25 23:14:07 +0530

    added build

---
+++ build/precache-manifest.3842dc568625f7b4b299eda5c3ebb530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "866c898b714b6e9e847c52822805c9ae",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "7e7c28d6183f4c436b59",
+    "url": "/react/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/react/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "7e7c28d6183f4c436b59",
+    "url": "/react/static/js/main.da74f541.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
